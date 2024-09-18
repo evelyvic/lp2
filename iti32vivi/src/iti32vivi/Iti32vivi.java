adfadbf621ce56ad4a860dc862a7e4f816517cff
evelyvic <evelyvicsn@gmail.com>
2024-09-18 11:56:14 -0300

    Add files via upload

---
+++ iti32vivi/src/iti32vivi/Iti32vivi.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package iti32vivi;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Iti32vivi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Carro carro = new Carro("Toyota","Corolla");
+        carro.andar();
+        // ira exibir "O carro Toyota Corolla está andando".
+    }
+    
+}
+
+class Carro {
+    private String marca;
+    private String nome; 
+    public Carro(String marca, String nome){
+        this.marca = marca;
+        this.nome = nome;
+         }
+    public void andar(){
+        System.out.println("O carro " + marca + " " + nome + " esta andando");
+        
+    }
+}
