2c0e016a07f8e4201dd1e7a249e65080b35bb2c5
evelyvic <evelyvicsn@gmail.com>
2024-10-02 11:52:52 -0300

    Update Iti32vivi.java

--- iti32vivi/src/iti32vivi/Iti32vivi.java
+++ iti32vivi/src/iti32vivi/Iti32vivi.java
@@ -1,9 +1,7 @@
-    /**
+/**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        /*
+       /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
@@ -27,8 +25,8 @@ public class JavaApplication5 {
     
 }
 class Carro {
-  private String marca;
-  private String nome;
+  private final String marca;
+  private final String nome;
   public Carro(String marca, String nome) {
     this.marca = marca;
     this.nome = nome;
