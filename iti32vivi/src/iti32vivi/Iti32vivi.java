e3e260ed9fe6a291fc3b7d428a0f57f4f450db35
evelyvic <evelyvicsn@gmail.com>
2024-10-02 11:50:15 -0300

    iti32vivi.java

--- iti32vivi/src/iti32vivi/Iti32vivi.java
+++ iti32vivi/src/iti32vivi/Iti32vivi.java
@@ -1,15 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package mariavaleteprojeto;
-
-/**
- *
- * @author Aluno
- */
-public class MariaValeteprojeto {
-
     /**
      * @param args the command line arguments
      */
