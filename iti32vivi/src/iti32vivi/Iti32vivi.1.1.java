8302084eabea4dbf39f81f01e7a1e76c88b7cea3
evelyvic <evelyvicsn@gmail.com>
2024-10-02 11:45:47 -0300

    iti32vivi.1.1.java

--- iti32vivi/src/iti32vivi/Iti32vivi.1.1.java
+++ iti32vivi/src/iti32vivi/Iti32vivi.1.1.java
@@ -2,20 +2,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication1;
+package javaapplication5;
 
 /**
  *
  * @author Aluno
  */
-public class JavaApplication1 {
+public class JavaApplication5 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // TODO code application logic here
+      Carro carro = new Carro("Fiat", "Gol");
+    carro.marca = "VW"; // Irá apresentar um erro de compilação
+    carro.andar(); 
+    }
+  lass Carro {
+  private String marca;
+  private String nome;
+  public Carro(String marca, String nome) {
+    this.marca = marca;
+    this.nome = nome;
+  }
+  protected String nomeCompleto() {
+    return marca + " " + nome;
+  }
+  public void andar() {
+    System.out.println("O carro " + nomeCompleto() + " está andando");
+  }
+}
 
-class Caminhao extends Carro {
+  class Caminhao extends Carro {
     private int carga;
     public Caminhao(String marca, String nome, int carga) {
         super(marca, nome);
@@ -25,30 +44,4 @@ class Caminhao extends Carro {
         System.out.println("O caminhão " + super.nomeCompleto() +
         " carrega " + carga + " toneladas");
     }
-}
-public class App {
-    public static void main(String[] args) {
-        Caminhao caminhao = new Caminhao("Mercedes", "1318", 16);
-        // O caminhão Mercedes 1318 carrega 16 toneladas
-        caminhao.mostraCarga();
-    }
-}
-
-class Caminhao extends Carro {
-    private int carga;
-    public Caminhao(String marca, String nome, int carga) {
-        super(marca, nome);
-        this.carga = carga;
-    }
-    public void andar() {
-        System.out.println("O caminhão " + super.nomeCompleto() +
-        " está carregando " + carga + " toneladas");
-    }
-    public void andar(boolean deRe) {
-        if (deRe)
-            System.out.println("O caminhão " + super.nomeCompleto() +
-            " está carregando " + carga + " toneladas de ré");
-        else
-            andar();
-    }
-}
+  }
